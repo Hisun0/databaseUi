2f4522343ef88b795424694799654fbac2756bec
Hisun0 <tridvad1@gmail.com>
2023-11-16 20:54:24 +0300

    refactor: add loading spinner

--- src/pages/PostList.tsx
+++ src/pages/PostList.tsx
@@ -1,30 +1,38 @@
 import { useSelector } from "react-redux";
-import { useAppDispatch } from "../slices";
+import { useAppDispatch, useAppSelector } from "../slices";
 import { fetchPosts, selectors } from "../slices/postsSlice";
 import { useEffect } from "react";
-import PostInterface from "../models/posts.interface";
 import Main from "../layouts/Main";
 import PostItem from "../components/PostItem";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const PostList = () => {
   const dispatch = useAppDispatch();
 
+  const loadingStatus = useAppSelector((state) => state.posts.loadingStatus);
+
   const posts = useSelector(selectors.selectAll);
+
+  const renderContent = () => {
+    switch (loadingStatus) {
+      case "loading":
+        return <LoadingSpinner />;
+      case "failed":
+        return <p>Error loading user</p>;
+      case "idle":
+      default:
+        return posts.map((post, index) => <PostItem key={index} post={post} />);
+    }
+  };
 
   useEffect(() => {
     dispatch(fetchPosts());
-  }, []);
-
-  console.log(posts);
+  }, [dispatch]);
 
   return (
     <Main>
       <h1>Posts</h1>
-      <ul className="list__main">
-        {posts.map((post: PostInterface, index) => (
-          <PostItem key={index} post={post} />
-        ))}
-      </ul>
+      <ul className="list__main">{renderContent()}</ul>
     </Main>
   );
 };
