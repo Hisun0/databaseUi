09a27a56d925aa4f1a297ac1c20fdab02bad60c3
Hisun0 <tridvad1@gmail.com>
2023-11-16 20:53:44 +0300

    refactor: add new visual style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,20 @@
+import Main from "../layouts/Main";
+import errorImage from "../assets/404.svg";
+
 const NotFound = () => {
-  return <div>ooops, page that you are looking not exists</div>;
+  return (
+    <Main>
+      <div className="flex-col align-center">
+        <img src={errorImage} className="img-fluid" alt="404" />
+        <h1 className="color-green">
+          Let's back to the{" "}
+          <a href="/" className="color-blue">
+            main page
+          </a>
+        </h1>
+      </div>
+    </Main>
+  );
 };
 
 export default NotFound;
