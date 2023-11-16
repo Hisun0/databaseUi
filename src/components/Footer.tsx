8a724ebbe312a34ffa3005d04b9860c2c0db304d
Hisun0 <tridvad1@gmail.com>
2023-11-16 20:36:20 +0300

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="flex-row">
+          <p>Made with love by Rodion</p>
+          <a href="">Github</a>
+          <a href="">Telegram</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
