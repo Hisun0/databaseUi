3040699f610e00f6363510116b1aac1443761a32
Hisun0 <tridvad1@gmail.com>
2023-11-16 20:53:25 +0300

    refactor: add new visual style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import Main from "../layouts/Main";
+import databaseSvg from "../assets/database.svg";
 
 const Home = () => {
   return (
     <Main>
-      <h1>Welcome to DummyJSON visualiser!</h1>
-      <h2>How to start?</h2>
+      <div className="flex-row">
+        <div className="position-relative">
+          <h1>
+            Welcome to Database<span className="color-blue">UI</span>
+          </h1>
+          <h2>Your data, your way</h2>
+          <button className="btn btn-primary btn-large position-bottom">
+            Start editing
+          </button>
+        </div>
+        <img src={databaseSvg} className="img-fluid" alt="databaseImage" />
+      </div>
     </Main>
   );
 };
