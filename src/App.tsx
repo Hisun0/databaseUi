f0e82a21f70d332445f2ef9a538887e4db19a75d
Hisun0 <tridvad1@gmail.com>
2023-11-16 20:57:53 +0300

    refactor: add classes for a tags

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import PostRoutes from "./PostRoutes";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Navigation>
-        <Link id="1" to="/">
+        <Link id="1" to="/" className="a__header">
           Home
         </Link>
-        <Link id="2" to="/posts">
+        <Link id="2" to="/posts" className="a__header">
           Posts
         </Link>
       </Navigation>
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/posts/*" element={<PostRoutes />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </>
   );
 };
