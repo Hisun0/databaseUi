5380b7275cf15c2916c74ff27c3802dfa4b121c7
Hisun0 <tridvad1@gmail.com>
2023-11-19 14:13:44 +0300

    refactor: update routes

--- src/slices/routes.ts
+++ src/slices/routes.ts
@@ -1,4 +1,5 @@
 export default {
-  tasksPath: () => "https://dummyjson.com/posts",
-  taskPath: (id: string | number) => `https://dummyjson.com/posts/${id}`,
+  postsPath: () => "/api/posts",
+  postPath: (id: string) => `/api/posts/${id}`,
+  postAddPath: () => "/api/posts/add",
 };
