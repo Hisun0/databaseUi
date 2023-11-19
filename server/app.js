b9a65a903995c57a3803da13c5a407a8fa9deccf
Hisun0 <tridvad1@gmail.com>
2023-11-19 14:03:14 +0300

    refactor: server setup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/postsRouter.js";
+import bodyParser from "body-parser";
+
+mongoose
+  .connect("mongodb://localhost:27017/myDB")
+  .then(() => console.log("DB connected"))
+  .catch(() => console.log("ne poshlo"));
 
 const app = express();
-
 const PORT = 8080;
+
+app.use(express.static("dist"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(router);
 
 app.listen(PORT, () => console.log("Server started"));
