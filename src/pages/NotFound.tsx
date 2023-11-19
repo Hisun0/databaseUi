84183ab0680255f5743870d6af08e804292dbd43
Hisun0 <tridvad1@gmail.com>
2023-11-19 14:16:55 +0300

    refactor: immediate return in arrow function

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,20 +1,19 @@
 import Main from "../layouts/Main";
 import errorImage from "../assets/404.svg";
+import { Link } from "react-router-dom";
 
-const NotFound = () => {
-  return (
-    <Main>
-      <div className="flex-col align-center">
-        <img src={errorImage} className="img-fluid" alt="404" />
-        <h1 className="color-green">
-          Let's back to the{" "}
-          <a href="/" className="color-blue">
-            main page
-          </a>
-        </h1>
-      </div>
-    </Main>
-  );
-};
+const NotFound = () => (
+  <Main>
+    <div className="flex-col align-center">
+      <img src={errorImage} className="img-fluid" alt="404" />
+      <h1 className="color-green">
+        Let's back to the{" "}
+        <Link to="/" className="color-blue">
+          main page
+        </Link>
+      </h1>
+    </div>
+  </Main>
+);
 
 export default NotFound;
