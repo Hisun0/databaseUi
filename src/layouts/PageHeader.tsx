90cf03f536b93432f5194da37a275119ef22fe9d
Hisun0 <tridvad1@gmail.com>
2023-11-19 14:20:41 +0300

    feat: add new layout PageHeader

---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface PageHeaderProps {
+  children: ReactNode;
+  buttonText?: string;
+  navigatePath?: string;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({
+  children,
+  buttonText,
+  navigatePath,
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex-row align-center mb-1">
+      {children}
+      <button
+        className="btn btn-primary mb-1"
+        onClick={() => navigate(navigatePath ?? "/posts")}
+      >
+        {buttonText ?? "Back to posts"}
+      </button>
+    </div>
+  );
+};
+
+export default PageHeader;
