87a6ed413e4aefaccc5be9e80e3a0661695cd134
Hisun0 <tridvad1@gmail.com>
2023-11-19 14:16:28 +0300

    refactor: immediate return in arrow function

--- src/pages/NewPost.tsx
+++ src/pages/NewPost.tsx
@@ -1,5 +1,16 @@
-const NewPost = () => {
-  return <div>creating a new post</div>;
-};
+import FormContainer from "../components/FormContainer";
+import Main from "../layouts/Main";
+import PageHeader from "../layouts/PageHeader";
+
+const NewPost = () => (
+  <Main>
+    <PageHeader>
+      <h1>Create post</h1>
+    </PageHeader>
+    <div className="card">
+      <FormContainer />
+    </div>
+  </Main>
+);
 
 export default NewPost;
