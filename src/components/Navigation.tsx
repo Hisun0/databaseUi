af5a24eabfde5f6ae774d811b7ac68e771025a04
Hisun0 <tridvad1@gmail.com>
2023-11-19 14:10:55 +0300

    refactor: immediate return in arrow function

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,27 +5,21 @@ interface NavigationProps {
   children: ReactNode[];
 }
 
-const Navigation: React.FC<NavigationProps> = (props) => {
-  const { children } = props;
-
-  return (
-    <header>
-      <div className="container">
-        <div className="flex-row">
-          <img src={logoImage} alt="logo" />
-          <nav>
-            <ul className="list__header">
-              {children.map((child, index) => (
-                <li key={index} className="li__list__header">
-                  {child}
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
-  );
-};
+const Navigation: React.FC<NavigationProps> = ({ children }) => (
+  <header className="mb-1">
+    <div className="container flex-row">
+      <img src={logoImage} alt="logo" />
+      <nav>
+        <ul className="list__header">
+          {children.map((child, index) => (
+            <li key={index} className="li__list__header">
+              {child}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  </header>
+);
 
 export default Navigation;
