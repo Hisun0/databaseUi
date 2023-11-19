00a03b5de8653014095a7665c03edb860112a289
Hisun0 <tridvad1@gmail.com>
2023-11-19 14:18:50 +0300

    refactor: add handler if posts page empty

--- src/pages/PostList.tsx
+++ src/pages/PostList.tsx
@@ -1,27 +1,49 @@
-import { useSelector } from "react-redux";
 import { useAppDispatch, useAppSelector } from "../slices";
-import { fetchPosts, selectors } from "../slices/postsSlice";
+import { deletePost, fetchPosts } from "../slices/postsSlice";
 import { useEffect } from "react";
 import Main from "../layouts/Main";
 import PostItem from "../components/PostItem";
 import LoadingSpinner from "../components/LoadingSpinner";
+import NotFound from "./NotFound";
+import { selectAllPosts } from "../slices/selectors";
+import emptyImage from "../assets/empty.svg";
+import PageHeader from "../layouts/PageHeader";
 
 const PostList = () => {
   const dispatch = useAppDispatch();
 
   const loadingStatus = useAppSelector((state) => state.posts.loadingStatus);
 
-  const posts = useSelector(selectors.selectAll);
+  const posts = useAppSelector(selectAllPosts);
+
+  const handleDeletePost = (id: string) => {
+    dispatch(deletePost(id));
+    return;
+  };
 
   const renderContent = () => {
     switch (loadingStatus) {
       case "loading":
         return <LoadingSpinner />;
       case "failed":
-        return <p>Error loading user</p>;
+        return <NotFound />;
       case "idle":
       default:
-        return posts.map((post, index) => <PostItem key={index} post={post} />);
+        return posts.length !== 0 ? (
+          <ul className="list__main">
+            {posts.map((post, index) => (
+              <PostItem
+                key={index}
+                post={post}
+                onDeletePost={handleDeletePost}
+              />
+            ))}
+          </ul>
+        ) : (
+          <div className="flex-col align-center">
+            <img src={emptyImage} className="img-fluid" alt="posts are empty" />
+          </div>
+        );
     }
   };
 
@@ -31,8 +53,10 @@ const PostList = () => {
 
   return (
     <Main>
-      <h1>Posts</h1>
-      <ul className="list__main">{renderContent()}</ul>
+      <PageHeader buttonText="Create new posts" navigatePath="/posts/new">
+        <h1>Posts</h1>
+      </PageHeader>
+      {renderContent()}
     </Main>
   );
 };
