d83dddfb381f4333fb31f56e0f73bddd5513017c
Hisun0 <tridvad1@gmail.com>
2023-11-27 10:08:08 +0300

    refactor: add link to posts in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
 import Main from "../layouts/Main";
 import databaseSvg from "../assets/database.svg";
 
-const Home = () => (
-  <Main>
-    <div className="flex-row">
-      <div className="position-relative">
-        <h1>
-          Welcome to Database<span className="color-blue">UI</span>
-        </h1>
-        <h2>Your data, your way</h2>
-        <button className="btn btn-primary btn-large position-bottom">
-          Start editing
-        </button>
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Main>
+      <div className="flex-row">
+        <div className="position-relative">
+          <h1>
+            Welcome to Database<span className="color-blue">UI</span>
+          </h1>
+          <h2>Your data, your way</h2>
+          <button
+            className="btn btn-primary btn-large position-bottom"
+            onClick={() => navigate("/posts")}
+          >
+            Start editing
+          </button>
+        </div>
+        <img src={databaseSvg} className="img-fluid" alt="databaseImage" />
       </div>
-      <img src={databaseSvg} className="img-fluid" alt="databaseImage" />
-    </div>
-  </Main>
-);
+    </Main>
+  );
+};
 
 export default Home;
