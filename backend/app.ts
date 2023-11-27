9153770b11bfac09ea4be4fa974de1bedb901db9
Hisun0 <tridvad1@gmail.com>
2023-11-27 15:09:55 +0300

    refactor: replace port and database url with env

--- backend/app.ts
+++ backend/app.ts
@@ -1,15 +1,21 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
+import "dotenv/config.js";
+
 import router from "./routes/postsRouter.ts";
 import bodyParser from "body-parser";
 
+const databaseUrl = process.env.DB_CONNECTION;
+
+if (!databaseUrl) throw new Error("Your connection string is not defined!");
+
 mongoose
-  .connect("mongodb://localhost:27017/myDB")
+  .connect(databaseUrl)
   .then(() => console.log("DB connected"))
-  .catch(() => console.log("ne poshlo"));
+  .catch(() => new Error("Database is not connected!"));
 
 const app: Express = express();
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 app.use(express.static("dist"));
 
